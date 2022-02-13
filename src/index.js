30b67b60d17e08bbd6573427f6c4001d28c683cd
HawiCaesar <brianhawi92@gmail.com>
2022-02-13 14:34:59 +0300

    get initial translations for login

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback='App is loading ...'>
+    <Suspense fallback='Store Front'>
       <App />
     </Suspense>
   </React.StrictMode>,
