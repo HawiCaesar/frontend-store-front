2f31a918360c6ec4522e01312b0a7e3e9b5a4c17
HawiCaesar <brianhawi92@gmail.com>
2022-02-15 14:10:30 +0300

    integrate formik, add translations

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { App } from "./App";
+import { Login } from "./Login";
 import { LanguageSelection } from "./LanguageSelection";
 
 export const AppRoutes = () => {
   return (
     <Router>
         <Routes>
-          <Route exact path="/" element={<App />} />
+          <Route exact path="/" element={<Login />} />
           <Route path="/language-selection" element={<LanguageSelection />} />
         </Routes>
     </Router>
---
+++ src/Login.js
@@ -0,0 +1,117 @@
+import { useTranslation } from 'react-i18next';
+import { Formik } from 'formik';
+
+export function Login() {
+  const { t } = useTranslation();
+  return (
+    <div className="flex flex-col h-screen bg-gray-100">
+        <div className="grid place-items-center mx-2 my-20 sm:my-auto">
+        
+            <div className="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 
+                px-6 py-10 sm:px-10 sm:py-6 
+                bg-white rounded-lg shadow-md lg:shadow-lg">
+                
+                <h2 className="text-center font-semibold text-3xl lg:text-4xl text-gray-800">
+                    {t('login.input.login')}
+                </h2>
+
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validate={values => {
+                        const errors = {};
+                        if (!values.email) {
+                        errors.email = t('login.validations.email.required');
+                        } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                        errors.email = t('login.validations.email.invalid');
+                        }
+
+                        if(!values.password) {
+                            errors.password = t('login.validations.password')
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                        }, 400);
+                    }}
+                    >
+                    {({
+                        values,
+                        handleChange,
+                        errors,
+                        touched,
+                        handleSubmit,
+                        handleBlur,
+                        isValid,
+                        dirty
+                    }) => {
+
+                        console.log(errors, isValid, touched)
+                        return (
+                        <form className="mt-10" onSubmit={handleSubmit}>
+                            <label htmlFor="email" className="block text-xs font-semibold text-gray-600 uppercase">{t('login.input.email')}</label>
+                            <input id="email" type="email" name="email" placeholder={t('login.input.email')} autoComplete="off"
+                                className="block w-full py-3 px-1 mt-2 
+                                text-gray-800 appearance-none 
+                                border-b-2 border-gray-100
+                                focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.email}
+                            />
+                            <span className='text-red-400'>{errors.email && touched.email && errors.email}</span>
+
+                            <label htmlFor="password" className="block mt-2 text-xs font-semibold text-gray-600 uppercase">{t('login.input.password')}</label>
+                            <input id="password" type="password" name="password" placeholder={t('login.input.password')} autoComplete="off"
+                                className="block w-full py-3 px-1 mt-2 mb-4
+                                text-gray-800 appearance-none 
+                                border-b-2 border-gray-100
+                                focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.password}
+                            />
+                            
+                            <span className='text-red-400'>{errors.password && touched.password && errors.password}</span>
+            
+                            <button type="submit"
+                                className="w-full py-3 mt-10 bg-gray-800 rounded-sm
+                                font-medium text-white uppercase
+                                focus:outline-none hover:bg-gray-700 hover:shadow-none disabled:opacity-75"
+                                disabled={!isValid || !dirty}
+                            >
+                                {t('login.input.login')}
+                            </button>
+
+                            
+                            <div className="sm:flex sm:flex-wrap mt-8 sm:mb-4 text-sm text-center">
+                                <a href="forgot-password" className="flex-2 underline">
+                                {t('login.forgotPassword')}
+                                </a>
+
+                                <p className="flex-1 text-gray-500 text-md mx-4 my-1 sm:my-auto">
+                                    {t('login.or')}
+                                </p>
+                    
+                                <a href="register" className="flex-2 underline">
+                                {t('login.createAccount')}
+                                </a>
+                            </div>
+                        </form>
+                    )
+                    }}
+                </Formik>
+
+
+                <a href="/language-selection" className="text-center flex-2 underline">
+                  {t('login.changeLanguage')}
+                </a>
+            </div>
+        </div>
+    </div>
+  );
+}
